1e6c2434ab8cef2e962904f854dcdacc9471555b
Eloah <eloah.ferreira.silva@escola.pr.gov.br>
2025-02-19 12:20:04 -0300

    tentativas novo

--- aula1.py
+++ aula1.py
@@ -4,10 +4,17 @@ chute = "5"
 print( "### Seja muito bem vinda! ###")
 print("Adivinhe o número secreto de 1 a 10")
 chute = int (input("Digite um número aqui: "))
-print("Sorteio:", sorteio)
 print ("Chute", chute)
 
-if (chute == sorteio):
-    print ("Parabéns, você acertou!")
-else:
-    print ("Está errado.")
+limite_tentativas = 3
+tentativa = 1
+while (limite_tentativas >= tentativa):
+    print ("Tentativa", tentativa)
+    chute = int (input ("Digite seu chute: "))
+    if (chute == sorteio):
+        print ("Parabéns,você acertou!")
+    elif (chute > sorteio):
+        print ("Chute um número menor!")
+    elif (chute < sorteio):
+        print ("Chute um número maior!")
+    tentativa = tentativa + 1
