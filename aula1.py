cdebb6f8d7ab8b4a703f14144eb10fcfad4e4864
Eloah <eloah.ferreira.silva@escola.pr.gov.br>
2025-02-27 11:07:07 -0300

    Menu arrumado

--- aula1.py
+++ aula1.py
@@ -9,15 +9,15 @@ opcao = int(input("Digite a opção desejada:"))
 if (opcao == 1):
     print("Você escolheu o Nível Fácil")
     numero_max = 10
-    limite_tentativas = 5
+    limite_tentativas = 3
 elif (opcao == 2):
     print("Você escolheu o Nível Médio")
     numero_max = 20
-    limite_tentativas = 8
+    limite_tentativas = 5
 elif (opcao == 3):
     print("Você escolheu o Nível Difícil")
     numero_max = 50
-    limite_tentativas = 3
+    limite_tentativas = 10
 else:
     print("Opção inválida")
     exit()
